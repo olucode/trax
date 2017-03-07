3ff71de99b363a189d3ef4257cf81becdda9225b
fortec007 <olumightyfalomo@gmail.com>
2017-03-08 00:01:20 +0100

    Add Pagination to all songs under genre

---
+++ app/Http/Controllers/SimpleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \App\Song;
+use Illuminate\Http\Request;
+
+class SimpleController extends Controller
+{
+    /**
+     * Display Homepage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showIndex()
+    {
+        $songs = Song::all();   //returns a Collection(object)
+        return view('trax.index', compact('songs'));
+    }
+
+    /**
+     * Display all the genres available
+     * @return \Illuminate\Http\Response
+     */
+    public function listGenres()
+    {
+        $genres = ['Hip Hop', 'Pop', 'Rap', 'AfroBeat', 'Electronic', 'Soul', 'Reggae', 'Heavy Metal'];
+        //$genres = Song::pluck('genre')->all();
+        return view('trax.showGenre', compact('genres'));
+    }
+
+    public function listSongsUnderCategory($genre)
+    {
+        $songs = Song::where('genre', $genre)->paginate(6);  //returns a Collection (object)
+        return view('trax.allSongsUnderGenre', compact(['songs', 'genre']));
+    }
+
+    /**
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showSong($id)
+    {
+        $song = Song::find($id);  //returns JSON 
+        //$song = Song::where('title', '=', $id)->get();
+        return view('layouts.song', compact('song'));
+        //return "I have a song... ".$id;
+    }
+
+}
