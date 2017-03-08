83501099e5fe774eeaaa602c4c8bb728a9d3ac99
fortec007 <olumightyfalomo@gmail.com>
2017-03-09 00:18:23 +0100

    Used ValidateSong Request to validate input.

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ValidateSong;
 use App\Song;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -22,41 +23,30 @@ class SongController extends Controller
         return view('songs.create');
     }
 
-    public function store(Request $request)
+    public function store(ValidateSong $request)
     {
 
         $newSong = $request->all(); //get all form input
-        $rules = [
-            'title' => 'required',
-            'artist' => 'required',
-            'year' => 'required|numeric',
-            'producer' => 'required'
-        ];  //validation rules
-        $validator = Validator::make($newSong,$rules);
-        if ($validator->fails()) {
-            return redirect('/songs/create')->withErrors($validator)->withInput();
-        }
-        else {
-            $fileName = $request->file('image')->store('images');
-            $data = [
-                'title' => $newSong['title'],
-                'artist' => $newSong['artist'],
-                'album' => $newSong['album'],
-                'year' => $newSong['year'],
-                'producer' => $newSong['producer'],
-                'comment' => $newSong['comment'],
-                'genre' => $newSong['genre'],
-                'image' => $fileName
-            ];
-            Song::create($data);
-            //return "Successful";
-            return view('trax.addSongSuccess');
-        }
+        $fileName = $request->file('image')->store('images');
+        $data = [
+            'title' => $newSong['title'],
+            'artist' => $newSong['artist'],
+            'album' => $newSong['album'],
+            'year' => $newSong['year'],
+            'producer' => $newSong['producer'],
+            'comment' => $newSong['comment'],
+            'genre' => $newSong['genre'],
+            'image' => $fileName
+        ];
+        Song::create($data);
+        //return "Successful";
+        return view('trax.addSongSuccess');
+
     }
 
     public function show($id)
     {
-        //I'm using SImpleController@showSong since a user doesn't have to be logged in to view a song
+        //I'm using SimpleController@showSong since a user doesn't have to be logged in to view a song
     }
 
     public function edit($id)
@@ -80,8 +70,7 @@ class SongController extends Controller
         if ($validator->fails()) {
             return "Did not pass";
             //return redirect()->route('songs.edit',$id)->withErrors($validator);
-        }
-        else{
+        } else{
             Song::find($id)->update($updatedSong); //update the song
             $song = Song::find($id);
             if ($updatedSong->hasFile('image')) {
