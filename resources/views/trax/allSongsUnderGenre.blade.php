2d9eacc2978e1a8054c4e344e6746c7adb30ef2f
fortec007 <olumightyfalomo@gmail.com>
2017-06-23 09:07:59 +0100

    Renamed layout file appropriately
    This is the beginning of the great rewrite. I would be rewriting certain parts, modifying the folder structure.

--- resources/views/trax/allSongsUnderGenre.blade.php
+++ resources/views/trax/allSongsUnderGenre.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.navbar')
+@extends('layouts.master')
 
 @section('content')
 
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+Route::get('/', 'SimpleController@showIndex');
+
 Route::get(
     'songs/category/{genre}',
     ['as' => 'genre', 'uses' => 'SimpleController@listSongsUnderCategory']
 );
 Route::get('showSong/{id}', ['as' => 'song', 'uses' => 'SimpleController@showSong']);
 Route::resource('songs', 'SongController');
-Route::get('/', 'SimpleController@showIndex');
 Route::get('profile', 'HomeController@showProfile');
 Route::get('allgenres', 'SimpleController@listGenres');
 //Route::get('songs/category/{genre}/{id}', 'SongController');
