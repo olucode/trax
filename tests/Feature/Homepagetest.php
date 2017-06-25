5b4f8f55903ef22233f65cbdd070a0b8a8f8dcf7
fortec007 <olumightyfalomo@gmail.com>
2017-06-25 11:47:56 +0100

    Added config for testing
    Added the database to use for testing.Created test for homepage

--- tests/Feature/Homepagetest.php
+++ tests/Feature/Homepagetest.php
@@ -4,18 +4,15 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class Homepagetest extends TestCase
 {
-
-    use DatabaseMigrations;
     /**
-     * A basic functional test example.
+     * A basic test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testExample()
     {
-        $this->visit('/')
-             ->see('T R A X');
+        $this->assertTrue(true);
     }
 }
